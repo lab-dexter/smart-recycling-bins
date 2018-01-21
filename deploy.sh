dee73fda6784b91c1a801a6efea79bd1916aad89
Ignas Asmontas <ignas.asmontas@gmail.com>
2018-01-21 19:11:07 +0000

    Minor improvements

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# This is initial script for smart recycle bin software deployment. 
+#
+
 
 #======================================
 # Deployment dependency variables
@@ -12,28 +16,36 @@ DIR="smart-recycling-bin"
 SERVICE_FILE="recycle-bin.service"
 #======================================
 
+echo "#===== Started deployment of ${DIR} software =====#"
 
 rm -rf ./${DIR} && \
-  echo "----- Removed ./${DIR} directory -----"
+  echo "#----- Removed ./${DIR} directory -----#"
 yes | git clone git@github.com:lab-dexter/smart-recycling-bins.git ${DIR}
 
-echo "===== Deploying dependency packages, etc. ====="
-python3 -m venv ${PENV} && \
-  echo "----- Successfully created virtual python env: ${PENV}"
+echo "#===== Deploying dependency packages, etc. =====#"
+# TODO: Add debian package check/install for: python3-venv
+# e.g.: dpkg -l python3-venv | grep python3-venv 
+
+source ${PENV}/bin/activate || \
+  echo "Virtual python environment \'${PENV}\' not found. Creating..." && \
+  python3 -m venv ${PENV} && \
+  echo "#----- Successfully created virtual python env: ${PENV} -----#"
 source ${PENV}/bin/activate
-pip install -r "${DIR}/requirements.txt"
+pip install -r "${DIR}/config/requirements.txt"
+
+
+echo "#===== Setting up service related files =====#"
 
 # chmod u+x "${DIR}/monitor.py"
-
 sudo cp ${DIR}/${SERVICE_FILE} /lib/systemd/system/ && \
-  echo "----- Copied over new service file -----"
+  echo "#----- Copied over new service file -----#"
 
 sudo systemctl daemon-reload && \
-  echo "----- Reloaded daemon config -----"
+  echo "#----- Reloaded daemon config -----#"
 
 sudo systemctl enable ${SERVICE_FILE} && \
-  echo "----- Enabled ${SERVICE_FILE} service -----"
+  echo "#----- Enabled ${SERVICE_FILE} service -----#"
 
 sudo systemctl start ${SERVICE_FILE} && \
-  echo "----- Started ${SERVICE_FILE} service -----"
+  echo "#----- Started ${SERVICE_FILE} service -----#"
 
