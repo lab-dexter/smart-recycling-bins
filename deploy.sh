b1bf138a75e793813673380cd2ba22603a4ccb89
Ignas Asmontas <Ignas@Ignass-MBP.Dlink>
2018-02-02 22:15:24 +0200

    Added branch variable

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
-#
+#------------------------------------------------------------------------------------------
 # This is initial script for smart recycle bin software deployment. 
-#
+# Arguments accepted:
+#   1 -> BRANCH - repo branch to clone to. Defaults to 'master' branch if not specified
+#------------------------------------------------------------------------------------------
 
+BRANCH=${1:-"master"}
 
-#======================================
-# Deployment dependency variables
-#======================================
+#------------------------------------------------------------------------------------------
+# Parameters
+
 PENV="srb"
+DPKG_LIST=(python-setuptools)
 
-#======================================
 # recycle-bin.service deployment variables
-#======================================
 DIR="smart-recycling-bin"
 SERVICE_FILE="recycle-bin.service"
-#======================================
-
-#======================================
-DPKG_LIST=(python-setuptools)
+#--------------------------------------------------------
 
 echo "==================================================="
 echo "Started deployment of ${DIR} software"
 echo "==================================================="
+echo ""
+echo "Target branch is: ${BRANCH}"
+echo "---------------------------------------------------"
 
 rm -rf ./${DIR} && \
-  echo "#----- Removed ./${DIR} directory -----#"
-yes | git clone git@github.com:lab-dexter/smart-recycling-bins.git ${DIR}
+  echo "Removed ./${DIR} directory"
+yes | git clone -b ${BRANCH} git@github.com:lab-dexter/smart-recycling-bins.git ${DIR}
 
 echo "==================================================="
 echo "Deploying dependency packages, etc."
@@ -48,25 +50,19 @@ source ${PENV}/bin/activate || \
 #  python3 -m venv ${PENV} && \
   virtualenv ${PENV}
   echo "==================================================="
-  echo "Successfully created virtual python env: ${PENV}"
+  echo " Successfully created virtual python env: ${PENV}"
   echo "==================================================="
 
 source ${PENV}/bin/activate
 pip install -r "${DIR}/config/requirements.txt"
 
 echo "==================================================="
-echo "Setting up service related files"
+echo " Setting up service related files"
 echo "==================================================="
 # chmod u+x "${DIR}/monitor.py"
 sudo cp ${DIR}/${SERVICE_FILE} /lib/systemd/system/ && \
   echo "Copied over new service file"
 
-sudo systemctl daemon-reload && \
-  echo "Reloaded daemon config"
-
-sudo systemctl enable ${SERVICE_FILE} && \
-  echo "Enabled ${SERVICE_FILE} service"
-
-sudo systemctl start ${SERVICE_FILE} && \
-  echo "Started ${SERVICE_FILE} service"
-
+sudo systemctl daemon-reload
+sudo systemctl enable ${SERVICE_FILE} 
+sudo systemctl start ${SERVICE_FILE} 
