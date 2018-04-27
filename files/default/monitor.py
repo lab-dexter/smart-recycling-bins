a624f9b6e27c972c49217c1af2f87dbbae70d22c
KipsasJaujoj <vaidotasslavinskas@gmail.com>
2018-04-27 17:30:56 +0300

    api call to send data to openshift added

--- files/default/monitor.py
+++ files/default/monitor.py
@@ -1,19 +1,24 @@
 import RPi.GPIO as GPIO
 import cayenne.client
 import time
+from datetime import datetime
 import ConfigParser
 import ast
+import requests
+import os
 GPIO.setmode(GPIO.BCM)
 
 
 class SmartBin():
-    def __init__(self, mqtt_username, mqtt_password, mqtt_client_id, trig, echo_pins, sleep_time, debug = True):
+    def __init__(self, mqtt_username, mqtt_password, mqtt_client_id, trig, echo_pins, sleep_time, api_url, eth_MAC, debug=True):
         self.mqtt_username = mqtt_username
         self.mqtt_password = mqtt_password
         self.mqtt_client_id = mqtt_client_id
         self.trig = trig
         self.echo_pins = echo_pins
         self.sleep_time = sleep_time
+        self.api_url = api_url
+        self.eth_MAC = eth_MAC
         self.debug = debug
         client = None
         self.setup()
@@ -33,9 +38,9 @@ class SmartBin():
         # GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
 
         self.log_msg("Distance measurement in progress")
-        GPIO.setup(self.trig,GPIO.OUT)
+        GPIO.setup(self.trig, GPIO.OUT)
         for i in self.echo_pins:
-            GPIO.setup(self.echo_pins[i],GPIO.IN)
+            GPIO.setup(self.echo_pins[i], GPIO.IN)
         self.res()
 
     def res(self):
@@ -61,7 +66,7 @@ class SmartBin():
                 for j in xrange(5):
                     self.res()
                     k = 0
-                    while GPIO.input(self.echo_pins[i])==0:
+                    while GPIO.input(self.echo_pins[i]) == 0:
                         pulse_start = time.time()
                         k += 1
                         if k > 200:
@@ -69,7 +74,7 @@ class SmartBin():
                             break
 
                     k = 0
-                    while GPIO.input(self.echo_pins[i])==1:
+                    while GPIO.input(self.echo_pins[i]) == 1:
                         pulse_end = time.time()
                         k += 1
                         if k > 200:
@@ -101,14 +106,40 @@ class SmartBin():
         """Saves provided data to database"""
         self.log_msg("Sensor %s shows distance: %s cm" % (sensor_number, distance))
         self.client.virtualWrite(int(sensor_number), distance)
+        try:
+            timestamp = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S")
+            data = {"id": sensor_number, "mac": self.eth_MAC, "data": distance, "time": timestamp}
+            requests.post(self.api_url, data=data)
+        except:
+            pass
 
     def run_it(self):
         self.client = cayenne.client.CayenneMQTTClient()
         self.client.on_message = self.on_message
         self.client.begin(self.mqtt_username, self.mqtt_password, self.mqtt_client_id)
-        i=0
-        timestamp = 0
         self.monitor()
+
+
+def get_eth_name():
+    # Get name of the Ethernet interface
+    try:
+        for root, dirs, files in os.walk('/sys/class/net'):
+            for dir in dirs:
+                if dir[:3] == 'enx' or dir[:3] == 'eth':
+                    interface = dir
+    except:
+        interface = "None"
+    return interface
+
+
+def get_MAC(interface='eth0'):
+    # Return the MAC address of the specified interface
+    try:
+        str = open('/sys/class/net/%s/address' % interface).read()
+    except:
+        str = "00:00:00:00:00:00"
+    return str[0:17]
+
 
 if __name__ == '__main__':     # Program start from here
     config = ConfigParser.ConfigParser()
@@ -119,12 +150,17 @@ if __name__ == '__main__':     # Program start from here
     MQTT_CLIENT_ID = config.get('default', 'MQTT_CLIENT_ID')
     TRIG = config.getint('default', 'TRIG')
     ECHO_PINS = ast.literal_eval(config.get('default', 'ECHO_PINS'))
-    smart_bin = SmartBin(mqtt_username = MQTT_USERNAME,
-                         mqtt_password = MQTT_PASSWORD,
-                         mqtt_client_id = MQTT_CLIENT_ID,
-                         trig = TRIG,
-                         echo_pins = ECHO_PINS,
-                         sleep_time = sleep_time)
+    api_url = config.get('default', 'api_url')
+    eth_name = get_eth_name()
+    eth_MAC = get_MAC(eth_name)
+    smart_bin = SmartBin(mqtt_username=MQTT_USERNAME,
+                         mqtt_password=MQTT_PASSWORD,
+                         mqtt_client_id=MQTT_CLIENT_ID,
+                         trig=TRIG,
+                         echo_pins=ECHO_PINS,
+                         sleep_time=sleep_time,
+                         api_url=api_url,
+                         eth_MAC=eth_MAC)
     try:
         smart_bin.run_it()
     except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
